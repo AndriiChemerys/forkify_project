c479bee4893b1cda8b30ae0a1678e87554443c9c
AndriiChemerys <andriichemerys@gmail.com>
2020-09-10 21:32:54 +0200

    Building the Search Model

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,3 +1,19 @@
-export default 'I am an exported string.';
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
 
-const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+    async getResults(query) {
+        try {
+            const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+            console.log(res)
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
+
+
+// const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
