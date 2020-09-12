3ddb081a8f313840986f81a80d916ae7805b8055
AndriiChemerys <andriichemerys@gmail.com>
2020-09-12 10:11:31 +0200

    Building the Search Model

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,17 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-async function getResults(query) {
-    const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
-    console.log(res)
-}
-getResults('pizza');
+// async function getResults(query) {
+//     const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+//     console.log(res)
+// }
+// getResults('pizza');
 
 // Recipe
 // const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+
+import Search from './models/Search';
+
+const search = new Search('pizza');
+console.log(search);
+
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,19 +1,23 @@
+import axios from 'axios';
+
 export default class Search {
     constructor(query) {
         this.query = query;
     }
 
-    async getResults(query) {
+    async getResults() {
         try {
-            const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
-            console.log(res)
+            const res = await axios(
+                `https://forkify-api.herokuapp.com/api/search?&q=${this.query}`
+            );
+            console.log(res);
             this.result = res.data.recipes;
-            // console.log(this.result);
+            console.log(this.result);
+            // console.log(recipes);
         } catch (error) {
             alert(error);
         }
     }
 }
-
 
 // const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
