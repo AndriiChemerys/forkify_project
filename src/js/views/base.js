610d8982c6968cfdcb0dc1cb71ba49c7ccb93af8
AndriiChemerys <andriichemerys@gmail.com>
2020-10-08 20:54:38 +0200

    Render Loader

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -3,3 +3,16 @@ export const elements = {
     searchInput: document.querySelector('.search__field'),
     searchResList: document.querySelector('.results__list'),
 }
+
+
+
+export const renderLoader = parent => {
+    const loader = `
+        <div class="${elementStrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
+    `;
+    parent.insertAdjacentHTML('afterbegin', loader);
+};
